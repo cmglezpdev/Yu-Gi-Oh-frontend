6532ec431d518e6f03536b73ea01fc3d039a2010
Carlos Manuel González <cmglezpdev@gmail.com>
2023-10-07 19:49:11 -0400

    chore add config to use tailwind + primevue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import PrimeVue from 'primevue/config'
 import router from '@/router/router'
 import AppVue from './App.vue'
 
+// import 'primevue/resources/themes/md-dark-deeppurple/theme.css'
+import 'primevue/resources/themes/md-light-deeppurple/theme.css'
+import 'primeicons/primeicons.css'
+
 const app = createApp(AppVue)
 
 app.use(router)
