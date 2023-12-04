4126be415f44400984b1195b970c477dce44f08f
Alex Sanchez Saez <alessq732@gmail.com>
2023-12-03 22:18:42 -0500

    resolviendo conflictos

--- src/utils/admin.users.service.ts
+++ src/utils/admin.users.service.ts
@@ -1,72 +1,6 @@
-<<<<<<< HEAD
-export async function getusersFromPanel(){
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        resolve([
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-          {
-            name:'usuario',
-            email:'user@mail.com',
-            role:'admin',
-          },
-        ])
-      }, 2000);
-    })
-}
-export async function createUserFromPanel(){
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        resolve({})
-      }, 2000);
-    })
-}
-export async function editUserFromPanel(){
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        resolve({})
-      }, 2000);
-    })
-}
-export async function removeUserFromPanel(){
-  return new Promise(
-    (resolve, reject) => {
-      setTimeout(() => {
-        resolve({})
-      }, 2000);
-    })
-}
-=======
 import { httpClient } from "./axios";
->>>>>>> 039b63d2c96b4dc7028bae2a30840e9c9477ddc6
 
-const getAllUser = async () => {
+export const getusersFromPanel = async () => {
   const users: any = [];
 
   const response = await httpClient.get(`user`);
@@ -87,7 +21,7 @@ export async function fetchUser(): Promise<any[]> {
   return new Promise(
     (resolve) => {
       resolve(
-        getAllUser()
+        getusersFromPanel()
       )
     }
   )
